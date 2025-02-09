87fe3eacab4034b033cdd1ddb797563d0f558f3a
Diego Castillo <diegocasmo@gmail.com>
2025-02-09 18:22:45 -0500

    Go back to using default Resend provider email

--- src/lib/auth/resend-provider.ts
+++ src/lib/auth/resend-provider.ts
@@ -1,12 +1,14 @@
 import type { EmailConfig } from "next-auth/providers/email";
 import Resend from "next-auth/providers/resend";
-import { Resend as ResendClient } from "resend";
 
-const consoleLogVerificationRequest: EmailConfig["sendVerificationRequest"] =
-  async ({ identifier, url, provider }) => {
-    const { host } = new URL(url);
+const logVerificationRequest: EmailConfig["sendVerificationRequest"] = async ({
+  identifier,
+  url,
+  provider,
+}) => {
+  const { host } = new URL(url);
 
-    console.log(`
+  console.log(`
 ----------------------------------
 From: ${provider.from}
 To: ${identifier}
@@ -18,38 +20,6 @@ ${url}
 
 ----------------------------------
   `);
-  };
-
-const sendVerificationRequest: EmailConfig["sendVerificationRequest"] = async ({
-  identifier,
-  url,
-  provider,
-}) => {
-  const { host } = new URL(url);
-  const resend = new ResendClient(process.env.AUTH_RESEND_KEY);
-
-  const result = await resend.emails.send({
-    from: provider.from!,
-    to: identifier,
-    subject: `Sign in to ${host}`,
-    html: `
-      <body>
-        <h1>Sign in to ${host}</h1>
-        <p>Click the link below to sign in:</p>
-        <p>
-          <a href="${url}" target="_blank" rel="noopener noreferrer">
-            Sign in
-          </a>
-        </p>
-        <p>If the button doesn't work, you can also copy and paste this link:</p>
-        <p>${url}</p>
-      </body>
-    `,
-  });
-
-  if (result.error) {
-    throw new Error(result.error.message);
-  }
 };
 
 export const getResendProvider = () => {
@@ -63,9 +33,9 @@ export const getResendProvider = () => {
 
   return Resend({
     from: process.env.EMAIL_FROM,
-    sendVerificationRequest:
-      process.env.NODE_ENV === "development"
-        ? consoleLogVerificationRequest
-        : sendVerificationRequest,
+    // Send email verification requests to the console in dev to avoid spamming the Resend API
+    ...(process.env.NODE_ENV === "development"
+      ? { sendVerificationRequest: logVerificationRequest }
+      : {}),
   });
 };
