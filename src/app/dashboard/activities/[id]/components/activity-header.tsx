75b38745496b301bdc1ebf71cd48761ba2dcba68
Diego Castillo <diegocasmo@gmail.com>
2025-02-23 18:24:44 -0500

    Fix activity header

--- src/app/dashboard/activities/[id]/components/activity-header.tsx
+++ src/app/dashboard/activities/[id]/components/activity-header.tsx
@@ -10,21 +10,23 @@ type ActivityHeaderProps = {
 
 export function ActivityHeader({ activity }: ActivityHeaderProps) {
   return (
-    <div className="flex justify-between items-start">
-      <div className="flex items-start space-x-6">
-        <Button variant="outline" size="icon" asChild className="mt-2">
+    <div className="flex justify-between ">
+      <div className="flex items-center space-x-6">
+        <Button variant="outline" size="icon" asChild>
           <Link href="/dashboard" aria-label="Back to dashboard">
             <ChevronLeft className="h-4 w-4" />
           </Link>
         </Button>
-        <div className="min-h-[88px]">
+        <div className="flex-col">
           <h1 className="text-4xl font-bold flex items-center">
             <ActivityIcon className="w-10 h-10 mr-4 text-primary" />
             {activity.name}
           </h1>
-          <p className="mt-2 text-xl text-muted-foreground h-7 overflow-hidden">
-            {activity.description}
-          </p>
+          {activity.description && (
+            <p className="mt-2 text-xl text-muted-foreground h-7 overflow-hidden">
+              {activity.description}
+            </p>
+          )}
         </div>
       </div>
       <div className="flex items-center space-x-2 sm:space-x-4">
