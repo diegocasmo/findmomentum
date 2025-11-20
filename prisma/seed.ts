ff2b8b6a49ed5412ae11c56f5c5c09bbd550a93c
Diego Castillo <diegocasmo@gmail.com>
2025-11-20 17:09:36 -0500

    chore(package.json): upgrade Prisma (#202)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 import { faker } from "@faker-js/faker";
 import { findOrCreateDefaultTeam } from "@/lib/services/find-or-create-default-team";
 import { createActivity } from "@/lib/services/create-activity";
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL || "";
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   // Create a new user with verified email
